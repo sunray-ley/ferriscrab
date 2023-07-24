a9e0f03c7454252a6a47f8623615e573cd328c6f
sunray-ley <sunray.ley@gmail.com>
2023-07-24 16:41:25 +0800

    feat: add TCP listening and HTTP request/response handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
