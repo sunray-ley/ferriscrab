27f9065f12730ec005fbcbd983e7ec1b8fb552b9
sunray-ley <sunray.ley@gmail.com>
2023-07-26 15:59:35 +0800

    feat: create ThreadPool struct

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
+use ferriscrab::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    let thread_pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        thread_pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -23,10 +29,13 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
     let request_line = http_request.first().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "examples/resources/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "examples/resources/404.html")
+    let (status_line, filename) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "examples/resources/hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "examples/resources/hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "examples/resources/404.html"),
     };
 
     let body = fs::read_to_string(filename).unwrap();
