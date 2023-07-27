febe123e827884b4ca7c81dbf9f04d18f1b84d5f
sunray-ley <sunray.ley@gmail.com>
2023-07-27 11:13:04 +0800

    feat: create Worker struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,45 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
-    pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+
+        Self { workers }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Self {
+        let thread = thread::spawn(|| {});
+
+        Self { id, thread }
     }
 }
