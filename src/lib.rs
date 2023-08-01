4eb96a55b9257a1f1dc299645bf8a89d9fce29ca
sunray-ley <sunray.ley@gmail.com>
2023-08-01 11:09:25 +0800

    feat: use channels to send requests to threads

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -15,13 +19,17 @@ impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        Self { workers }
+        Self { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -37,9 +45,13 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Self {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
 
         Self { id, thread }
     }
 }
+
+struct Job;
